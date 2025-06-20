4283b6fb33a0c2671bf4eb1d2dad153a65bf68a7
Yosefario <168045758+yosefario-dev@users.noreply.github.com>
2025-06-20 17:46:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="freeproxy",
+    name="proxly",
     version="0.1.0",
     author="Yosefario Dev",
     author_email="github@yosefario.me",
