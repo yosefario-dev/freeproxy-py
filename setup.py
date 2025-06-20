9f20f379c9f99195b48f8836b0b2029a0c5a473c
Yosefario <168045758+yosefario-dev@users.noreply.github.com>
2025-06-20 17:32:33 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="freeproxy",
+    version="0.1.0",
+    author="Yosefario Dev",
+    author_email="github@yosefario.me",
+    description="HTTP proxy library for Python using CroxyProxy infrastructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yosefario-dev/freeproxy",
+    py_modules=["freeproxy"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "curl-cffi",
+    ],
+)
