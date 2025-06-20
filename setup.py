a22f2976400dcbd5b4a05c459249dfd927b44d6e
Yosefario <168045758+yosefario-dev@users.noreply.github.com>
2025-06-20 23:38:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="proxly",
-    version="0.1.2",
+    version="0.1.3",
     author="Yosefario Dev",
     author_email="github@yosefario.me",
     description="HTTP proxy library for Python using CroxyProxy infrastructure",
